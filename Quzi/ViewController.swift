024d59fe2960f8b7879f08aded09a6b4517d9b11
Anshul Gupta <hi.anshulg@gmail.com>
2020-08-23 13:26:11 -0700

    Initial Commit

---
+++ Quzi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quzi
+//
+//  Created by Anshul Gupta on 8/23/20.
+//  Copyright © 2020 Anshul Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
